e3dd66af10069d0b8e6e0a45021d05c96a66feb3
Emerson Barina <emersonbarina@gmail.com>
2022-12-27 17:52:26 -0300

    Create Class Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo_theme/src/utils/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,21 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: MaterialColor(0xFFFFE200, <int, Color>{
-            50 : Color(0x1AFFE200),
-            100 : Color(0x33FFE200),
-            200 : Color(0x4DFFE200),
-            300 : Color(0x66FFE200),
-            400 : Color(0x80FFE200),
-            500 : Color(0xFFFFE200),
-            600 : Color(0x99FFE200),
-            700 : Color(0xB3FFE200),
-            800 : Color(0xCCFFE200),
-            900 : Color(0xE6FFE200),
-          })
-      ),
+      theme: TAppTheme.lighTheme,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
