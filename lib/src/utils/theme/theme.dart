19fd0a9a6d0812db5c0591110ea3fdb7ce8ae4f4
Emerson Barina <emersonbarina@gmail.com>
2022-12-27 19:22:34 -0300

    Creating TTextTheme

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,29 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'text_theme.dart';
 
 class TAppTheme {
   static ThemeData lighTheme = ThemeData(
     brightness: Brightness.light,
-    textTheme: TextTheme(
-      headline2: GoogleFonts.montserrat(
-        color: Colors.black87,
-      ),
-      subtitle1: GoogleFonts.poppins(
-        color: Colors.black54,
-        fontSize: 24,
-      ),
-    ),
+    textTheme: TTextTheme.lighTextTheme,
   );
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
-    textTheme: TextTheme(
-      headline2: GoogleFonts.montserrat(
-        color: Colors.white70,
-      ),
-      subtitle1: GoogleFonts.poppins(
-        color: Colors.white60,
-        fontSize: 24,
-      ),
-    ),
+    textTheme: TTextTheme.darkTextTheme,
   );
 }
