4b5c58578d0a4bca3fc27277677a8a3dc071d89a
Emerson Barina <emersonbarina@gmail.com>
2022-12-27 19:29:07 -0300

    Final Learning

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-import 'text_theme.dart';
+import 'widgets_themes/text_theme.dart';
 
 class TAppTheme {
+
+  TAppTheme._();
+
   static ThemeData lighTheme = ThemeData(
     brightness: Brightness.light,
     textTheme: TTextTheme.lighTextTheme,
+    elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom())
   );
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
