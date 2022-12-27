c0b31267a3b22fe143d7bd6ae1c4e1d6792f0b7b
Emerson Barina <emersonbarina@gmail.com>
2022-12-27 17:44:11 -0300

    Using MaterialColor

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.light, primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(brightness: Brightness.dark, ),
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: MaterialColor(0xFFFFE200, <int, Color>{
+            50 : Color(0x1AFFE200),
+            100 : Color(0x33FFE200),
+            200 : Color(0x4DFFE200),
+            300 : Color(0x66FFE200),
+            400 : Color(0x80FFE200),
+            500 : Color(0xFFFFE200),
+            600 : Color(0x99FFE200),
+            700 : Color(0xB3FFE200),
+            800 : Color(0xCCFFE200),
+            900 : Color(0xE6FFE200),
+          })
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
       // definition of the theme
       themeMode: ThemeMode.system,
       home: const AppHome(),
     );
   }
 }
-
 
 class AppHome extends StatelessWidget {
   const AppHome({Key? key}) : super(key: key);
@@ -39,12 +54,22 @@ class AppHome extends StatelessWidget {
             const Text("Heding"),
             const Text("Sub-heding"),
             const Text("Paragraph"),
-            ElevatedButton(onPressed: (){}, child: Text("Elevated"),),
-            OutlinedButton(onPressed: (){}, child: Text("outlined"),),
-            TextButton(onPressed: (){}, child: Text("Text Button"),),
-            const Padding( 
-                padding: const EdgeInsets.all(20.0),
-              child: Image(image: AssetImage("assets/images/icon-transparent700.png")),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Elevated"),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text("outlined"),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text("Text Button"),
+            ),
+            const Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Image(
+                  image: AssetImage("assets/images/icon-transparent700.png")),
             ),
           ],
         ),
@@ -56,4 +81,3 @@ class AppHome extends StatelessWidget {
     );
   }
 }
-
