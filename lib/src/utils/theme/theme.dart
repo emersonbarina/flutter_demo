2e3595f556e68c06c87c7221d98392fd121c17c4
Emerson Barina <emersonbarina@gmail.com>
2022-12-27 19:05:55 -0300

    Using TextTheme in Theme.dart

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TAppTheme {
-  static ThemeData lighTheme = ThemeData( brightness: Brightness.light );
-  static ThemeData darkTheme = ThemeData( brightness: Brightness.dark );
+  static ThemeData lighTheme = ThemeData(
+    brightness: Brightness.light,
+    textTheme: TextTheme(
+      headline2: GoogleFonts.montserrat(
+        color: Colors.black87,
+      ),
+      subtitle1: GoogleFonts.poppins(
+        color: Colors.black54,
+        fontSize: 24,
+      ),
+    ),
+  );
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    textTheme: TextTheme(
+      headline2: GoogleFonts.montserrat(
+        color: Colors.white70,
+      ),
+      subtitle1: GoogleFonts.poppins(
+        color: Colors.white60,
+        fontSize: 24,
+      ),
+    ),
+  );
 }
