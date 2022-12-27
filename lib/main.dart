05af26ce75b45c611908d626a90fdbdca1f0fe71
Emerson Barina <emersonbarina@gmail.com>
2022-12-27 17:20:28 -0300

    definition of the theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.light),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      // definition of the theme
+      themeMode: ThemeMode.system,
       home: const AppHome(),
     );
   }
