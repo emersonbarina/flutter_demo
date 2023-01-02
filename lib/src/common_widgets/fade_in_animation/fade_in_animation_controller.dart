ada753b5e3296e64ab8860f25a130120fc2b8170
Emerson Barina <emersonbarina@gmail.com>
2023-01-02 20:04:58 -0300

    Animate any widget - splash screen with new class TFadeInAnimation

--- lib/src/common_widgets/fade_in_animation/fade_in_animation_controller.dart
+++ lib/src/common_widgets/fade_in_animation/fade_in_animation_controller.dart
@@ -8,11 +8,19 @@ class FadeInAnimationController extends GetxController{
 
   RxBool animate = false.obs;
 
+  Future startSplashAnimation() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    animate.value = true;
+    await Future.delayed(const Duration(milliseconds: 3000));
+    animate.value = false;
+    await Future.delayed(const Duration(milliseconds: 5000));
+    Get.offAll(() => const WelcomeScreen());
+  }
+
   Future startAnimation() async {
     await Future.delayed(const Duration(milliseconds: 500));
     animate.value = true;
-    await Future.delayed(const Duration(milliseconds: 5000));
-    Get.to(const WelcomeScreen());
+    Get.offAll(() => const WelcomeScreen());
   }
 
 }
