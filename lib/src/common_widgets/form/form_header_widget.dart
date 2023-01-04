01c2356def6221da41355f9bfd5497063019cd4f
Emerson Barina <emersonbarina@gmail.com>
2023-01-04 12:34:13 -0300

    Forget Password - improvement of FormHeaderWidget

--- lib/src/common_widgets/form/form_header_widget.dart
+++ lib/src/common_widgets/form/form_header_widget.dart
@@ -5,31 +5,43 @@ class FormHeaderWidget extends StatelessWidget {
       {Key? key,
       required this.image,
       required this.title,
-      required this.subTitle})
+      required this.subTitle,
+      this.imageColor,
+      this.imageHeight = 0.2,
+      this.heightBetween,
+      this.crossAxisAlignment = CrossAxisAlignment.start,
+      this.textAlign})
       : super(key: key);
 
+  final Color? imageColor;
+  final double imageHeight;
+  final double? heightBetween;
   final String image, title, subTitle;
+  final CrossAxisAlignment crossAxisAlignment;
+  final TextAlign? textAlign;
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: crossAxisAlignment,
       children: [
         Image(
           image: AssetImage(image),
-          height: size.height * 0.2,
+          color: imageColor,
+          height: size.height * imageHeight,
         ),
+        SizedBox(height: heightBetween,),
         Text(
           title,
           style: Theme.of(context).textTheme.headline1,
         ),
         Text(
           subTitle,
+          textAlign: textAlign,
           style: Theme.of(context).textTheme.bodyText1,
         ),
       ],
     );
   }
 }
-
