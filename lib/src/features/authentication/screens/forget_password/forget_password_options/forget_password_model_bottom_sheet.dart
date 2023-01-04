36b2d3b677ef3d0743968569d984237e1decbd89
Emerson Barina <emersonbarina@gmail.com>
2023-01-04 12:03:42 -0300

    Forget Password - Extract method and created file/class

---
+++ lib/src/features/authentication/screens/forget_password/forget_password_options/forget_password_model_bottom_sheet.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../constants/sizes.dart';
+import '../../../../../constants/text_strings.dart';
+import 'forget_password_btn_widget.dart';
+
+class ForgetPasswordScreen {
+  static Future<dynamic> buildShowModalBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      builder: (context) => Container(
+        padding: const EdgeInsets.all(tDefaultSize),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              tForgetPasswordTitle,
+              style: Theme.of(context).textTheme.headline2,
+            ),
+            Text(
+              tForgetPasswordSubTitle,
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            ForgetPasswordBtnWidget(
+              onTap: () {},
+              btnIcon: Icons.mail_outline_outlined,
+              title: tEmail,
+              subTitle: tForgetPasswordResetViaEmail,
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ForgetPasswordBtnWidget(
+              onTap: () {},
+              btnIcon: Icons.mobile_friendly_outlined,
+              title: tPhoneNumber,
+              subTitle: tForgetPasswordResetViaPhone,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/src/features/authentication/screens/login/widgets/login_form_widget.dart
+++ lib/src/features/authentication/screens/login/widgets/login_form_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../../../constants/sizes.dart';
 import '../../../../../constants/text_strings.dart';
-import '../../forget_password/forget_password_options/forget_password_btn_widget.dart';
+import '../../forget_password/forget_password_options/forget_password_model_bottom_sheet.dart';
 
 class LoginForm extends StatelessWidget {
   const LoginForm({
@@ -46,44 +46,7 @@ class LoginForm extends StatelessWidget {
               alignment: Alignment.centerRight,
               child: TextButton(
                 onPressed: () {
-                  showModalBottomSheet(
-                    context: context,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    builder: (context) => Container(
-                      padding: const EdgeInsets.all(tDefaultSize),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tForgetPasswordTitle,
-                            style: Theme.of(context).textTheme.headline2,
-                          ),
-                          Text(
-                            tForgetPasswordSubTitle,
-                            style: Theme.of(context).textTheme.bodyText1,
-                          ),
-                          const SizedBox(
-                            height: 30.0,
-                          ),
-                          ForgetPasswordBtnWidget(
-                            onTap: (){},
-                            btnIcon: Icons.mail_outline_outlined,
-                            title: tEmail,
-                            subTitle: tForgetPasswordResetViaEmail,
-                          ),
-                          const SizedBox(height: 20.0,),
-                          ForgetPasswordBtnWidget(
-                            onTap: (){},
-                            btnIcon: Icons.mobile_friendly_outlined,
-                            title: tPhoneNumber,
-                            subTitle: tForgetPasswordResetViaPhone,
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
+                  ForgetPasswordScreen.buildShowModalBottomSheet(context);
                 },
                 child: const Text(tForgetPassword),
               ),
@@ -101,4 +64,3 @@ class LoginForm extends StatelessWidget {
     );
   }
 }
-
