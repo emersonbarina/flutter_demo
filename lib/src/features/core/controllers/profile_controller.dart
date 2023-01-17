a7d8a4cdda609ee792bcc03d1db2f24d7ad68555
emersonbarina <emersonbarina@gmail.com>
2023-01-17 13:30:50 -0300

    How to fetch data from Cloud Firestore - Step 3

---
+++ lib/src/features/core/controllers/profile_controller.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_demo_theme/src/repository/authentication_repository/authentication_repository.dart';
+import 'package:get/get.dart';
+
+import '../../../repository/user_repository/user_repository.dart';
+
+class ProfileController extends GetxController {
+  static ProfileController get instance => Get.find();
+
+  final _authRepo = Get.put(AuthenticationRepository());
+  final _userRepo = Get.put(UserRepository());
+
+  getUserData() {
+    final email = _authRepo.firebaseUser.value?.email;
+    if(email != null){
+      return _userRepo.getUserDetails(email);
+    } else {
+      Get.snackbar("Error", "Login to continue");
+    }
+  }
+
+}
